c4340d0ecbe20f18bea2224b5f557884ff2a31d2
Jan Paw <jpaw@virtuslab.com>
2016-02-20 22:41:40 +0100

    ref mes in SP7021

--- quartz.go
+++ quartz.go
@@ -75,6 +75,7 @@ func out(l *lps331ap.LPS331AP, b *bh1750.BH1750, s *si7021.SI7021) <-chan string
 				"%s quantity=tmp value=%f\n%s quantity=press value=%f\n%s quantity=lux value=%f\n%s quantity=rh value=%f",
 				*mes, temp, *mes, press, *mes, lux, *mes, rh)
 			tmp <- str
+			Trace.Println(str)
 			time.Sleep(time.Duration(*tick) * time.Second)
 		}
 	}()
